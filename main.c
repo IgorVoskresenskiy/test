003606284dde44be6c871a99514f9fb081c87868
IgorVoskresenskiy <123014354+IgorVoskresenskiy@users.noreply.github.com>
2023-01-19 21:07:36 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,41 @@
 #include "stdio.h"
 
+int inputCheck(char input[])
+{
+    if ((input[0] == 'G') && (input[1] == 'I') && (input[2] == 'V') && (input[3] == 'E') &&
+        (input[6] == 'M') && (input[7] == 'O') && (input[8] == 'N') && (input[9] == 'E') && (input[10] == 'Y'))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main()
 {
     char code[16];
-    gets(code);
-    if ((code[0] == 'G')&&(code[1] == 'I')&&(code[2] == 'V')&&(code[3] == 'E')&&(code[6] == 'M')&&(code[7] == 'O')&&(code[8] == 'N')&&(code[9] == 'E')&&(code[10] == 'Y'))
+    for (int i = 0; i < 16; i++)
+        code[i] = '\0';
+    fgets(code, 15, stdin);
+    if (inputCheck(code))
     {
         printf("coins: ");
-        if (code[11] != '/0') {
+        if (code[11] != '\0')
+        {
             printf("%c", code[11]);
         }
-       if (code[12] != '/0') {
+        if (code[12] != '\0')
+        {
             printf("%c", code[12]);
         }
-        if (code[13] != '/0') {
+        if (code[13] != '\0')
+        {
             printf("%c", code[13]);
         }
-        if (code[14] != '/0') {
+        if (code[14] != '\0')
+        {
             printf("%c", code[14]);
         }
     }
