9ed42a34a8934a9ff1dde226aaa61fa13af2a17f
IgorVoskresenskiy <123014354+IgorVoskresenskiy@users.noreply.github.com>
2023-01-24 22:45:17 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,49 @@
 #include "stdio.h"
+#include "stdbool.h"
 
-int check_input(char* input)
+#define IS_NUMBER_POW_OF_TWO(number) ((((number) > 0) && ((number) & ((number) - 1))) == 0)
+#define ASCII_0_DIGIT_OFFSET 0x30
+#define ASCII_9_DIGIT_OFFSET 0x39
+
+int ASCII_to_int(int asciiCode)
+{
+    return asciiCode - ASCII_0_DIGIT_OFFSET;
+}
+
+bool check_input(char* input)
 {
     int inputLength = strlen(input);
-    if ((inputLength>=12)&&(inputLength<=16)&&(input[0] == 'G') && (input[1] == 'I') && (input[2] == 'V') && (input[3] == 'E') &&
-        ((int)input[4] <= 57) && ((int)input[4] >= 48) && ((int)input[5] >= 48) && ((int)input[5] <= 57) &&
-            (input[6] == 'M') && (input[7] == 'O') && (input[8] == 'N') && (input[9] == 'E') && (input[10] == 'Y'))
+    bool isLengthOk = inputLength >= 12 && inputLength <= 16;
+
+    bool isNumbersOk = (int)input[4] <= ASCII_9_DIGIT_OFFSET && (int)input[4] >= ASCII_0_DIGIT_OFFSET &&
+        (int)input[5] >= ASCII_0_DIGIT_OFFSET && (int)input[5] <= ASCII_9_DIGIT_OFFSET;
+
+    bool isMaskOk = input[0] == 'G' && input[1] == 'I' && input[2] == 'V' && input[3] == 'E' &&
+            input[6] == 'M'&& input[7] == 'O'&& input[8] == 'N'&& input[9] == 'E'&& input[10] == 'Y';
+
+    bool isCoinAmountCorrect = (int)input[11] <= ASCII_9_DIGIT_OFFSET && (int)input[11] >= ASCII_0_DIGIT_OFFSET && 
+        (int)input[12] >= ASCII_0_DIGIT_OFFSET && (int)input[12] <= ASCII_9_DIGIT_OFFSET &&
+        (int)input[13] >= ASCII_0_DIGIT_OFFSET && (int)input[13] <= ASCII_9_DIGIT_OFFSET &&
+        (int)input[14] >= ASCII_0_DIGIT_OFFSET && (int)input[14] <= ASCII_9_DIGIT_OFFSET;
+
+    if (isLengthOk && isNumbersOk && isMaskOk)
     {
-        return 1;
+        return true;
     }
-    else
-    {
-        return 0;
-    }
+
+    return false;
 }
 
 int main()
 {
-    int multiplyer = 1, multiplyerCheck, digitCount=0, coinAmount=0;
+    int multiplyer = 1, cheatNumber, digitCount=0, coinAmount=0;
     char code[16] = { '\0' };
     fgets(code, 16, stdin);
     if (check_input(code))
     {
         printf("coins: ");
-        multiplyerCheck = ((int)code[4] - 48) * 10 + ((int)code[5] - 48);
-        if (multiplyerCheck > 0 && (multiplyerCheck & (multiplyerCheck - 1)) == 0)
+        cheatNumber = ASCII_to_int(code[4]) * 10 + ASCII_to_int(code[5]);
+        if (IS_NUMBER_POW_OF_TWO(cheatNumber))
         {
             multiplyer = 2;
         }
@@ -35,26 +54,12 @@ int main()
                 digitCount += 1;
             }
         }
-        if (digitCount == 1)
+        for (int i = 0; i < digitCount; i++)
         {
-            coinAmount = ((int)code[11]-48) * multiplyer;
-            printf("%d", coinAmount);
+            coinAmount *= 10;
+            coinAmount += ASCII_to_int(code[11 + i]);
         }
-        if (digitCount == 2)
-        {
-            coinAmount = (((int)code[11]-48) * 10 + ((int)code[12]-48)) * multiplyer;
-            printf("%d", coinAmount);
-        }
-        if (digitCount == 3)
-        {
-            coinAmount = (((int)code[11]-48) * 100 + ((int)code[12]-48) * 10 + ((int)code[13]-48)) * multiplyer;
-            printf("%d", coinAmount);
-        }
-        if (digitCount == 4)
-        {
-            coinAmount = (((int)code[11]-48) * 1000 + ((int)code[12]-48) * 100 + ((int)code[13]-48) * 10 + ((int)code[14]-48)) * multiplyer;
-            printf("%d", coinAmount);
-        }
+        printf("%d", coinAmount * multiplyer);
     }
     else
     {
