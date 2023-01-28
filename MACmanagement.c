53af4923556155d5677c583d7032965b31569d10
DESKTOP-93RNQIJ\msi <voskres04@gmail.com>
2023-01-28 21:41:35 +0500

    lowercase_to_uppercase function

--- MACmanagement.c
+++ MACmanagement.c
@@ -10,6 +10,7 @@
 #define ASCII_f_DIGIT_OFFSET 0x66
 #define ASCII_LOWERCASE_HEX_CONVERSION 0x57
 #define ASCII_UPPERCASE_HEX_CONVERSION 0x37
+#define ASCII_TO_UPPERCASE_CONVERSION 0x20
 
 static bool is_input_correct(char* input)
 {
@@ -22,6 +23,18 @@ static bool is_input_correct(char* input)
 			return true;
 		}
 		else return false;
+	}
+}
+
+static int lowercase_to_uppercase(char* input)
+{
+	for (int i = 0; i < 12; i++)
+	{
+		if (((int)input[i] >= ASCII_a_DIGIT_OFFSET) && ((int)input[i] <= ASCII_f_DIGIT_OFFSET))
+		{
+			char upperCaseLetter = (int)input[i] - ASCII_TO_UPPERCASE_CONVERSION;
+			input[i] = upperCaseLetter;
+		}
 	}
 }
 
@@ -39,10 +52,6 @@ static int mac_to_array(char* input, int* array)
 		{
 			firstDigit = ((int)input[i] - ASCII_UPPERCASE_HEX_CONVERSION);
 		}
-		if (((int)input[i] >= ASCII_a_DIGIT_OFFSET) && ((int)input[i] <= ASCII_f_DIGIT_OFFSET))
-		{
-			firstDigit = ((int)input[i] - ASCII_LOWERCASE_HEX_CONVERSION);
-		}
 		
 		if (((int)input[i+1] >= ASCII_0_DIGIT_OFFSET) && ((int)input[i+1] <= ASCII_9_DIGIT_OFFSET))
 		{
@@ -52,11 +61,7 @@ static int mac_to_array(char* input, int* array)
 		{
 			secondDigit = ((int)input[i+1] - ASCII_UPPERCASE_HEX_CONVERSION);
 		}
-		if (((int)input[i+1] >= ASCII_a_DIGIT_OFFSET) && ((int)input[i+1] <= ASCII_f_DIGIT_OFFSET))
-		{
-			secondDigit = ((int)input[i+1] - ASCII_LOWERCASE_HEX_CONVERSION);
-		}
-		
+
 		array[counter] = firstDigit * 16 + secondDigit;
 		counter++;
 	}
